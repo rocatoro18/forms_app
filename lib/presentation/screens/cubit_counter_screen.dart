4e47ca266d248722ed60e1bb7eef55399315cd1e
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2024-01-02 08:07:34 -0700

    Consumir y utilizar el CounterCubit

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_cubit/counter_cubit.dart';
 
 class CubitCounterScreen extends StatelessWidget {
   const CubitCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (_) => CounterCubit(), child: const _CubitCounterView());
+  }
+}
+
+class _CubitCounterView extends StatelessWidget {
+  const _CubitCounterView();
+
+  @override
+  Widget build(BuildContext context) {
+    // TAMBIEN AQUI SE PUEDEN ESTAR ESCUCHANDO LOS CAMBIOS QUE TIENE EL STATE
+    // CON EL WATCH CADA VEZ QUE EL ESTADO CAMBIE, SE VA A REDIBUJAR ESO
+    final counterState = context.watch<CounterCubit>().state;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cubit Counter'),
+        title: Text('Cubit Counter: ${counterState.transactionCount}'),
         actions: [
           IconButton(
               onPressed: () => {}, icon: const Icon(Icons.refresh_rounded))
         ],
       ),
-      body: const Center(
-        child: Text('Counter value: 0000'),
+      body: Center(
+        // BlocBuilder - Leer y construir cuando este CounterState cambia
+        child: BlocBuilder<CounterCubit, CounterState>(
+          // buildWhen - Contruir el widget solo cuando el CounterState es diferente al CounterState previo
+          // buildWhen: (previous, current) => current.counter != previous.counter,
+          builder: (context, state) {
+            return Text('Counter value: ${state.counter}');
+          },
+        ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
